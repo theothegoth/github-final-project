cf608766b3d95316ca77431ad97efaa7c47c262c
theothegoth <88789201+theothegoth@users.noreply.github.com>
2023-10-16 05:23:14 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # theothegoth
    # Input:
    # p, principal amount
    # t, time period in years
